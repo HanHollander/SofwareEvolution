848063400e9958357ba283347c240221b81dc838
Han Hollander <han_hollander@hotmail.nl>
2019-06-11 11:26:03 +0200

    git ignores Rplots.pdf now

--- assignment3/src/r/linear_regression.r
+++ assignment3/src/r/linear_regression.r
@@ -1,15 +1,38 @@
 
-dependent <- read.csv(file="../../data/csv/eclipse/dependent/change-review_period.csv",header=TRUE,sep=",");
+dependent <- read.csv(file="../../data/csv/eclipse/dependent/change-review_period.csv",header=TRUE,sep=",")
 
-predictor <- read.csv(file="../../data/csv/eclipse/predictor/change-tenure_change.csv",header=TRUE,sep=",");
+predictor <- read.csv(file="../../data/csv/eclipse/predictor/change-tenure_change.csv",header=TRUE,sep=",")
 
-merged <- merge(dependent, predictor, by = "ch_changeIdNum");
-# names(merged)[2];
+merged <- merge(dependent, predictor, by = "ch_changeIdNum")
+# names(merged)[2]
 
 plot(x=merged$DATEDIFF.hist2.hist_createdTime..hist1.hist_createdTime., y=merged$tenure_change,
     main="Review period ~ Change (ecosystem) tenure",
     type="p",
     log="")  # scatterplot
+
+merged_nozeros <- merged[apply(merged, 1, function(row) all(row != 0)),]
+
+plot(x=merged_nozeros$DATEDIFF.hist2.hist_createdTime..hist1.hist_createdTime., y=merged_nozeros$tenure_change,
+    main="Review period ~ Change (ecosystem) tenure",
+    type="p",
+    log="")  # scatterplot
+
+merged_noones <- merged_nozeros[apply(merged_nozeros, 1, function(row) all(row != 0)),]
+
+plot(x=merged_noones$DATEDIFF.hist2.hist_createdTime..hist1.hist_createdTime., y=merged_noones$tenure_change,
+    main="Review period ~ Change (ecosystem) tenure",
+    type="p",
+    log="")
+
+# ##Generate some data
+# dd <- data.frame(a = 1:4, b= 1:0, c=0:3)
+# print(dd)
+# ##Go through each row and determine if a value is zero
+# row_sub <- apply(dd, 1, function(row) all(row !=0 ))
+# print(row_sub)
+# ##Subset as usual
+# dd[row_sub,]
 
 # k <- c(1, 2, 3, 4);
 # v1 <- c("a", "b", "c", "d");
