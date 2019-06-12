1b30d3d8ce802eff594fa9ddfa357218a34ca1a7
Han Hollander <han_hollander@hotmail.nl>
2019-06-12 21:35:48 +0200

    generated new change-activity_change.csv

--- assignment3/src/mysql/predictor/change-activity_change.sql
+++ assignment3/src/mysql/predictor/change-activity_change.sql
@@ -1,16 +1,13 @@
 SELECT
 	ch1.ch_changeIdNum,
-    ch1.ch_authorAccountId,
     COUNT(ch1.ch_changeIdNum) as c
 FROM
--- 	gm_eclipse.t_change as ch1,
---  gm_eclipse.t_change as ch2
-	gm_libreoffice.t_change as ch1,
-    gm_libreoffice.t_change as ch2
+	gm_eclipse.t_change as ch1,
+	gm_eclipse.t_change as ch2
+-- 	gm_libreoffice.t_change as ch1,
+--  gm_libreoffice.t_change as ch2
 WHERE
 	ch1.ch_authorAccountId = ch2.ch_authorAccountId
     AND ch1.ch_createdTime > ch2.ch_createdTime
 GROUP BY
-	ch1.ch_changeIdNum, ch1.ch_authorAccountId
-ORDER BY
-	ch1.ch_authorAccountId, c
+	ch1.ch_changeIdNum
